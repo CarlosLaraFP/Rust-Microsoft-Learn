5d1c87d82648de3864ece771b860a72f7d21ec29
CarlosLaraFP <info@carloslaraai.com>
2023-05-08 09:57:08 -0700

    Implemented separate mod and imported struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod structs;
+
 use std::collections::HashMap;
 use rayon::prelude::*;
+use structs::Quote;
 
 fn main() {
     /*
@@ -99,21 +102,11 @@ fn main() {
 
     // Declare first variable binding with name "shadow_num"
     let shadow_num = 5;
-
     // Declare second variable binding, shadows existing variable "shadow_num"
     let shadow_num = shadow_num + 5;
-
     // Declare third variable binding, shadows second binding of variable "shadow_num"
     let shadow_num = shadow_num * 2;
-
     println!("The number is {}.", shadow_num);
-
-    struct Quote {
-        uuid: String,
-        amount: f32,
-        index: u8,
-        accepted: bool
-    }
 
     let quote = Quote { uuid: String::from("12345"), amount: 11.23, index: 1, accepted: true };
 
---
+++ src/structs.rs
@@ -0,0 +1,6 @@
+pub struct Quote {
+    pub uuid: String,
+    pub amount: f32,
+    pub index: u8,
+    pub accepted: bool
+}
