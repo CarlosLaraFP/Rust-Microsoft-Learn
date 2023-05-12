2a28c501fa101ea344a5555558ed2b07071ee746
CarlosLaraFP <info@carloslaraai.com>
2023-05-12 14:26:50 -0700

    Lifetime parameters for functions and references

--- src/main.rs
+++ src/main.rs
@@ -357,11 +357,35 @@ fn main() {
         }
     }
 
+    fn erase(_: String) { }
+
     #[derive(Debug)]
     struct Highlight<'document>(&'document str);
     let text = String::from("The quick brown fox jumps over the lazy dog.");
     let fox = Highlight(&text[4..19]);
     let dog = Highlight(&text[35..43]);
+    //erase(text);
+    //let new = text;
     println!("{:?}", fox);
     println!("{:?}", dog);
+
+    fn copy_and_return<'t>(vector: &mut Vec<String>, value: &'t str) -> &'t str {
+        vector.push(String::from(value));
+        value
+    }
+
+    let name1 = "Joe";
+    let name2 = "Chris";
+    let name3 = "Anne";
+
+    let mut names = Vec::new();
+
+    assert_eq!("Joe", copy_and_return(&mut names, name1));
+    assert_eq!("Chris", copy_and_return(&mut names, name2));
+    assert_eq!("Anne", copy_and_return(&mut names, name3));
+
+    assert_eq!(
+        names,
+        vec!["Joe".to_string(), "Chris".to_string(), "Anne".to_string()]
+    )
 }
