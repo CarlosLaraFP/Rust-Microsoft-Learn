2c5edb92e55c54251eee6ec594c4ec3b7decc0ff
CarlosLaraFP <info@carloslaraai.com>
2023-05-16 11:25:07 -0700

    Implemented generic type

--- src/main.rs
+++ src/main.rs
@@ -594,4 +594,35 @@ fn main() {
     assert_eq!(counter.next(), None);
 
     println!("Counter exhausted: {:#?}", counter);
+
+    for number in Counter::new(6) {
+        println!("{}", number);
+    }
+
+    let sum_until_10: usize = Counter::new(10).sum();
+    assert_eq!(sum_until_10, 55);
+
+    let powers_of_2: Vec<usize> = Counter::new(8).map(|n| 2usize.pow(n as u32)).collect();
+    assert_eq!(powers_of_2, vec![2, 4, 8, 16, 32, 64, 128, 256]);
+
+    /*
+        Transform a Container struct that only accepts positive integers of the
+        u32 type into a generic container that can hold values of any given type.
+     */
+    struct Container<T> {
+        value: T,
+    }
+    impl<T> Container<T> {
+        pub fn new(value: T) -> Self {
+            Container { value }
+        }
+    }
+
+    assert_eq!(Container::new(42).value, 42);
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
 }
