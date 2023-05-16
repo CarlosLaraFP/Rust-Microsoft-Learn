85e249fb8058f0ef43cb9125b5fc728423e3c151
CarlosLaraFP <info@carloslaraai.com>
2023-05-16 10:59:16 -0700

    Implemented Iterator trait for struct

--- src/main.rs
+++ src/main.rs
@@ -550,4 +550,48 @@ fn main() {
     };
 
     send_data_as_json(&kitty);
+
+    #[derive(Debug)]
+    struct Counter {
+        length: usize,
+        count: usize,
+    }
+
+    impl Counter {
+        fn new(length: usize) -> Counter {
+            Counter {
+                count: 0,
+                length,
+            }
+        }
+    }
+
+    impl Iterator for Counter {
+        type Item = usize;
+
+        fn next(&mut self) -> Option<Self::Item> {
+
+            self.count += 1;
+            if self.count <= self.length {
+                Some(self.count)
+            } else {
+                None
+            }
+        }
+    }
+
+    let mut counter = Counter::new(6);
+    println!("Counter just created: {:#?}", counter);
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), Some(6));
+    assert_eq!(counter.next(), None);
+    assert_eq!(counter.next(), None);  // further calls to `next` will return `None`
+    assert_eq!(counter.next(), None);
+
+    println!("Counter exhausted: {:#?}", counter);
 }
